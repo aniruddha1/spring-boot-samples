fa07b9e740fa5d19c42dfa32dfb7e0b1fe1bec62
Ani G <aniruddha.godawale@gmail.com>
2018-03-05 17:47:41 +0000

    initial

---
+++ consume-rest/src/main/java/com/aniruddha/ConsumeRestApplication.java
@@ -0,0 +1,12 @@
+package com.aniruddha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeRestApplication.class, args);
+	}
+}
