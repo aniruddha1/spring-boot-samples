cef485c76b24f4b5cc64a3c7d43c4aafb033f92c
Ani G <aniruddha.godawale@gmail.com>
2018-03-05 18:53:05 +0000

    first

---
+++ access-data-jpa/src/main/java/com/aniruddha/AccessDataJpaApplication.java
@@ -0,0 +1,12 @@
+package com.aniruddha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessDataJpaApplication.class, args);
+	}
+}
--- consume-rest/src/main/java/com/aniruddha/ConsumeRestApplication.java
+++ consume-rest/src/main/java/com/aniruddha/ConsumeRestApplication.java
@@ -1,7 +1,16 @@
 package com.aniruddha;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.aniruddha.domain.Quote;
+
 
 @SpringBootApplication
 public class ConsumeRestApplication {
@@ -9,4 +18,22 @@ public class ConsumeRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumeRestApplication.class, args);
 	}
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumeRestApplication.class);
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+
 }
+
