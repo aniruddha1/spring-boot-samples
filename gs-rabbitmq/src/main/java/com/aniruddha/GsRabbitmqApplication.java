16d97fcb82239906387b50ed08d66081c6728807
Ani G <aniruddha.godawale@gmail.com>
2018-03-12 19:24:03 +0000

    initial

---
+++ gs-rabbitmq/src/main/java/com/aniruddha/GsRabbitmqApplication.java
@@ -0,0 +1,12 @@
+package com.aniruddha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsRabbitmqApplication.class, args);
+	}
+}
